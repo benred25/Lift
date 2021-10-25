a471a9d0907f545aa4ff09e2f15457bf047c0f2a
benred25 <60947786+benred25@users.noreply.github.com>
2021-10-24 23:03:44 -0600

    more files generated by node

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// import routes .............
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT;
+
+// mongoose.connect ........
