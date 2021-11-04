8057a844d50da305f163e97d84ce3a866c6cdc91
benred25 <60947786+benred25@users.noreply.github.com>
2021-11-03 20:51:40 -0600

    lift table now filters out lifts without the right name.

--- client/src/components/Lifts/Lifts.js
+++ client/src/components/Lifts/Lifts.js
@@ -24,7 +24,7 @@ const Lifts = ({ currentId, setCurrentId }) => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {lifts.map((lift) => (
+                        {lifts.filter(lift => lift.exercise === "Bench Press").map((lift) => (
                             <TableRow>
                                 <TableCell>{lift.createdAt}</TableCell>
                                 <TableCell>{lift.sets}</TableCell>
